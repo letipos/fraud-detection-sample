a1939aa4397b0bfc21e168470f165427deb035ca
Leonard Posner <Leonard.Timo.Posner@ibm.com>
2023-04-14 14:25:22 +0200

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sample_python_project',
+    name='fraud_detection_project',
     version='0.1',
     packages=["fraud_detection_project",
               "fraud_detection_project.ml_functions"],

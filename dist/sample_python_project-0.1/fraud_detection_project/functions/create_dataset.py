cf8bd62d45239d00a56d7aa29735a818e6b19e62
Leonard Posner <Leonard.Timo.Posner@ibm.com>
2023-04-14 10:57:26 +0200

    new version

---
+++ dist/sample_python_project-0.1/fraud_detection_project/functions/create_dataset.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+import numpy as np
+
+# create a dataset with multiple features
+n_samples = 10000
+n_fraud_samples = 100
+n_normal_samples = n_samples - n_fraud_samples
+
+features = {
+    'age': np.random.randint(18, 70, size=n_samples),
+    'gender': np.random.choice(['M', 'F'], size=n_samples),
+    'income': np.random.randint(1000, 100000, size=n_samples),
+    'credit_score': np.random.randint(300, 900, size=n_samples),
+    'transaction_amount': np.random.randint(1, 10000, size=n_samples),
+    'merchant_category': np.random.choice(['Grocery', 'Gas', 'Restaurants', 'Entertainment'], size=n_samples),
+    'merchant_country': np.random.choice(['USA', 'Canada', 'Mexico'], size=n_samples),
+    'merchant_city': np.random.choice(['New York', 'Los Angeles', 'Toronto', 'Vancouver', 'Mexico City'], size=n_samples),
+    'transaction_time': np.random.randint(0, 24, size=n_samples),
+}
+
+fraud_indices = np.random.choice(n_samples, size=n_fraud_samples, replace=False)
+labels = np.zeros(n_samples)
+labels[fraud_indices] = 1
+
+df = pd.DataFrame(features)
+df['label'] = labels
+
+# save the dataset as a csv file
+df.to_csv('fraud_detection_data.csv', index=False)
---
+++ dist/sample_python_project-0.1/fraud_detection_project/functions/plots.py
@@ -0,0 +1,36 @@
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot(df):
+    # convert categorical variables to numerical labels
+    cat_features = ['gender', 'merchant_category', 'merchant_country', 'merchant_city']
+    for feature in cat_features:
+        df[feature] = df[feature].astype('category').cat.codes
+
+    # create a correlation matrix
+    corr = df.corr()
+
+    # create a heatmap of the correlation matrix
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(corr, cmap='coolwarm', annot=True, fmt='.2f')
+    plt.title('Correlation Matrix')
+
+    # create a pairplot of the numerical features
+    num_features = ['age', 'income', 'credit_score', 'transaction_amount']
+    plt.figure(figsize=(10, 8))
+    sns.pairplot(df[num_features], diag_kind='kde')
+    plt.suptitle('Pairplot of Numerical Features', y=1.02)
+
+    # create a countplot of the categorical features
+    fig, axs = plt.subplots(2, 2, figsize=(14, 10))
+    for i, feature in enumerate(cat_features):
+        row = i // 2
+        col = i % 2
+        sns.countplot(x=feature, data=df, ax=axs[row][col])
+        axs[row][col].set_title(f'Countplot of {feature}')
+        axs[row][col].set_xlabel('')
+        axs[row][col].set_ylabel('Count')
+        axs[row][col].tick_params(axis='x', labelrotation=90)
+    fig.tight_layout()
